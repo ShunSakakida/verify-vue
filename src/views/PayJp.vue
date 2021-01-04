3f781151b3ca6ff91b90788829b9c92816f27fdf
ShunSakakida <a5113067@outlook.jp>
2021-01-04 11:19:17 +0900

    payjp 支払い作成

--- src/views/PayJp.vue
+++ src/views/PayJp.vue
@@ -1,37 +1,56 @@
 <template>
     <div>
+        <!-- テスト公開鍵 -->
         <PayjpCheckout
-            api-key="pk_test_d4b1a4668323c8ce49ab7ec5"
-            client-id="d3d774f50bb006c26bac19402f0140a7228f8522"
+            api-key="pk_test_223acfaf29cfb280cdafedde"
             text="ポップアップを開く"
             submit-text="テストカードで支払い"
             name-placeholder="JOHN DOE"
             v-on:created="onTokenCreated"
             v-on:failed="onTokenFailed">
         </PayjpCheckout>
+        <div>
+            <p>token={{token}}</p>
+        </div>
+
+        <button @click="charge">売上を作る</button>
 
         <div ref="payjpArea"></div>
     </div>
 </template>
 <script>
-import PayjpCheckout from 'vue-payjp-checkout'
+import PayjpCheckout from 'vue-payjp-checkout';
+import axios from 'axios';
 
 export default {
+    // フロント側：公開鍵
     // https://titanwolf.org/Network/Articles/Article?AID=68d45874-1d28-4a27-952f-1dedf0cf6fe2#gsc.tab=0
-    // この記事
+    // https://qiita.com/hirotakasasaki/items/794c920016ac7c33da74
     // https://qiita.com/ma7ma7pipipi/items/de97e11bea5ba044f896
     // https://github.com/ngs/vue-payjp-checkout
     // npm install --save vue-payjp-checkout
+
+    // サーバ側：秘密鍵
+    // https://pay.jp/docs/api/#%E6%94%AF%E6%89%95%E3%81%84%E3%82%92%E4%BD%9C%E6%88%90
+
     components:{
         PayjpCheckout
     },
     methods:{
-        onTokenCreated:function(){
-            
+        onTokenCreated:function(res){
+            console.log(res.id);
+            this.token=res.id;            
         },
         onTokenFailed:function(){
 
         },
+        charge:function(){
+            let url='http://localhost:8080/payjp?token='+this.token;
+
+            axios.get(url).then(()=>{
+                console.log('一応終わった');
+            });
+        }
     },
     computed:{
     },
@@ -39,6 +58,7 @@ export default {
     },
     data() {
          return {
+             token:null,
          }
     },
 
